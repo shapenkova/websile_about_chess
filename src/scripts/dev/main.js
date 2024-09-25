aeffde136a04686dae96836be48ccd32d4f1999d
shapenkova <wondernasty@yandex.ru>
2024-09-25 21:37:58 +0500

    make a block stages, js

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -16,6 +16,19 @@ let autoScrollInterval;
 // Устанавка общего количества слайдов в элемент
 totalSlidesElement.textContent = totalSlides;
 
+function updateSlidesPerPage() {
+    if (window.innerWidth <= 1340) {
+        slidesPerPage = 1;
+    } 
+    else {
+        slidesPerPage = 3;
+    }
+    currentSlideIndex = Math.min(currentSlideIndex, totalSlides - slidesPerPage);
+    showSlides(currentSlideIndex);
+}
+
+window.addEventListener('resize', updateSlidesPerPage);
+
 // Обновление пагинации
 function updatePagination() {
     currentSlideElement.textContent =  currentSlideIndex + slidesPerPage;
@@ -23,7 +36,7 @@ function updatePagination() {
 function showSlides(startIndex) {
     slides.forEach((slide, index) => {
         slide.classList.add('hidden');
-        if (index >= startIndex && index < startIndex + 3) {
+        if (index >= startIndex && index < startIndex + slidesPerPage) {
             slide.classList.remove('hidden');
         }
     });
@@ -69,16 +82,70 @@ carousel.addEventListener('mouseenter', stopAutoScroll);
 carousel.addEventListener('mouseleave', startAutoScroll);
 
 // Инициализация показа первых трех слайдов и установки пагинации
+updateSlidesPerPage();
 showSlides(currentSlideIndex);
 startAutoScroll();
 
 
 document.addEventListener('scroll', function() {
-    var headerLink = document.querySelector('.page-header');
+    let headerLink = document.querySelector('.page-header');
     
-    if (window.scrollY > 50) { // Замените 50 на нужное значение
+    if (window.scrollY > 50) { 
         headerLink.classList.add('scrolled');
     } else {
         headerLink.classList.remove('scrolled');
     }
 });
+
+
+//Вторая карусель
+
+const wrapper = document.querySelector('.stages__wrapper');
+const sliderItems = Array.from(document.querySelectorAll('.stages__wrapper-slide'));
+let currentSlidesIndex = 0;
+
+
+function updateSlidesVisibility() {
+    const screenWidth = window.innerWidth;
+    
+    sliderItems.forEach(function (slide, index) {
+        if (screenWidth < 1020) {
+            if (index !== 0) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        } else {
+            slide.classList.remove('acrive');
+        }
+    });
+}
+
+updateSlidesVisibility();
+
+window.addEventListener('resize', updateSlidesVisibility);
+
+    // let currentIndex = 0;
+
+    // function updateSlide(index) {
+    //     const width = wrapper.clientWidth;
+    //     wrapper.scrollTo({
+    //         left: width * index,
+    //         behavior: 'smooth'
+    //     });
+    // }
+
+    // function nextSlide() {
+    //     const children = wrapper.children;
+    //     if (currentIndex < children.length - 1) {
+    //         currentIndex++;
+    //         updateSlide(currentIndex);
+    //     }
+    // }
+
+    // function previousSlide() {
+    //     if (currentIndex > 0) {
+    //         currentIndex--;
+    //         updateSlide(currentIndex);
+    //     }
+    // }
