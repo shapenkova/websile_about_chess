7ae9a9e82f80a4907f974f719278e315c50dd609
shapenkova <wondernasty@yandex.ru>
2024-09-26 10:54:28 +0500

    make a js

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//Слайдер циклический
 const carousel = document.getElementById('js-carousel');
 const slides = Array.from(document.querySelectorAll('.article-card'));
 const prevButton = document.querySelector('.carousel__arrow-prev');
@@ -13,23 +14,17 @@ let currentSlideIndex = 0;
 let slidesPerPage = 3;
 let autoScrollInterval;
 
-// Устанавка общего количества слайдов в элемент
 totalSlidesElement.textContent = totalSlides;
 
 function updateSlidesPerPage() {
-    if (window.innerWidth <= 1340) {
-        slidesPerPage = 1;
-    } 
-    else {
-        slidesPerPage = 3;
-    }
+    slidesPerPage = (window.innerWidth <= 1340) ? 1 : 3;
     currentSlideIndex = Math.min(currentSlideIndex, totalSlides - slidesPerPage);
     showSlides(currentSlideIndex);
 }
 
 window.addEventListener('resize', updateSlidesPerPage);
 
-// Обновление пагинации
+
 function updatePagination() {
     currentSlideElement.textContent =  currentSlideIndex + slidesPerPage;
 }
@@ -52,20 +47,16 @@ prevButton.addEventListener('click', () => {
 });
 
 function moveToNextSlide() {
-    if (currentSlideIndex + slidesPerPage < totalSlides) {
-        currentSlideIndex += slidesPerPage;
-    } else {
-        currentSlideIndex = 0;
-    }
+    currentSlideIndex = (currentSlideIndex + slidesPerPage < totalSlides) 
+        ? currentSlideIndex + slidesPerPage 
+        : 0;
     showSlides(currentSlideIndex);
 }
 
 function moveToPrevSlide() {
-    if (currentSlideIndex - slidesPerPage >= 0) {
-        currentSlideIndex -= slidesPerPage;
-    } else {
-        currentSlideIndex = totalSlides - slidesPerPage;
-    }
+    currentSlideIndex = (currentSlideIndex - slidesPerPage >= 0) 
+        ? currentSlideIndex - slidesPerPage 
+        : totalSlides - slidesPerPage;
     showSlides(currentSlideIndex);
 }
 
@@ -81,71 +72,14 @@ function stopAutoScroll() {
 carousel.addEventListener('mouseenter', stopAutoScroll);
 carousel.addEventListener('mouseleave', startAutoScroll);
 
-// Инициализация показа первых трех слайдов и установки пагинации
 updateSlidesPerPage();
 showSlides(currentSlideIndex);
 startAutoScroll();
 
-
+//Шапка сайта при скролле
 document.addEventListener('scroll', function() {
     let headerLink = document.querySelector('.page-header');
-    
-    if (window.scrollY > 50) { 
-        headerLink.classList.add('scrolled');
-    } else {
-        headerLink.classList.remove('scrolled');
-    }
+    window.scrollY > 50 
+        ? headerLink.classList.add('scrolled') 
+        : headerLink.classList.remove('scrolled');
 });
-
-
-//Вторая карусель
-
-const wrapper = document.querySelector('.stages__wrapper');
-const sliderItems = Array.from(document.querySelectorAll('.stages__wrapper-slide'));
-let currentSlidesIndex = 0;
-
-
-function updateSlidesVisibility() {
-    const screenWidth = window.innerWidth;
-    
-    sliderItems.forEach(function (slide, index) {
-        if (screenWidth < 1020) {
-            if (index !== 0) {
-                slide.classList.add('active');
-            } else {
-                slide.classList.remove('active');
-            }
-        } else {
-            slide.classList.remove('acrive');
-        }
-    });
-}
-
-updateSlidesVisibility();
-
-window.addEventListener('resize', updateSlidesVisibility);
-
-    // let currentIndex = 0;
-
-    // function updateSlide(index) {
-    //     const width = wrapper.clientWidth;
-    //     wrapper.scrollTo({
-    //         left: width * index,
-    //         behavior: 'smooth'
-    //     });
-    // }
-
-    // function nextSlide() {
-    //     const children = wrapper.children;
-    //     if (currentIndex < children.length - 1) {
-    //         currentIndex++;
-    //         updateSlide(currentIndex);
-    //     }
-    // }
-
-    // function previousSlide() {
-    //     if (currentIndex > 0) {
-    //         currentIndex--;
-    //         updateSlide(currentIndex);
-    //     }
-    // }
---
+++ src/scripts/dev/slider.js
@@ -0,0 +1,70 @@
+'use strict'
+
+// Вторая карусель
+const slider = document.querySelector('#stages-slider');
+const sliderItems = Array.from(document.querySelectorAll('.stages__wrapper-slide'));
+const btnNext = document.querySelector('#btnNext');
+const btnPrev = document.querySelector('#btnPrev');
+const paginationButtons = Array.from(document.querySelectorAll('.stages__pagination-btn'));
+let currentSlidesIndex = 0;
+const slidesToShow = 5;
+
+function updateSlidesVisibility() {
+    const screenWidth = window.innerWidth;
+
+    sliderItems.forEach(function (slide, index) {
+        slide.classList.toggle('active', screenWidth < 1020 ? index !== 0 : false);
+        
+        slide.dataset.index = index;
+    });
+    
+    sliderItems[currentSlidesIndex].setAttribute('data-active', '');
+    
+    updateButtonsState();
+    updatePaginationState();
+}
+
+function updateButtonsState() {
+    btnPrev.disabled = currentSlidesIndex === 0 ? true : false;
+    btnPrev.classList.toggle('arrow--disabled', currentSlidesIndex === 0);
+}
+
+function updatePaginationState() {
+    paginationButtons.forEach((button, index) => {
+        button.classList.toggle('active-btn', index === currentSlidesIndex);
+    });
+}
+
+btnNext.onclick = function () {
+    console.log('Next Slide');
+    showNextSlide('next');
+};
+
+btnPrev.onclick = function () {
+    console.log('Prev Slide');
+    showNextSlide('prev');
+};
+
+function showNextSlide(direction) {
+    const currentSlide = slider.querySelector('[data-active]');
+    const currentSlideIndex = +currentSlide.dataset.index;
+    currentSlide.classList.add('active');
+    currentSlide.removeAttribute('data-active');
+    
+    let nextSlideIndex;
+    nextSlideIndex = (direction === 'next') 
+    ? (currentSlideIndex + 1 >= sliderItems.length ? 0 : currentSlideIndex + 1)
+    : (currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1);
+
+    currentSlidesIndex = nextSlideIndex;
+
+    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+    nextSlide.classList.remove('active');
+    nextSlide.setAttribute('data-active', '');
+
+    updateButtonsState();
+    updatePaginationState();
+}
+
+updateSlidesVisibility();
+window.addEventListener('resize', updateSlidesVisibility);
